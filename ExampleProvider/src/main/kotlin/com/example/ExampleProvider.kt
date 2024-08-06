13ff4500cfed0f2863f3f929e858e356e751deda
Tran Hung <tranviethung912002@gmail.com>
2024-08-07 04:24:52 +0700

    vietsuborg

--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -1,19 +1,33 @@
 package com.example
 
+import com.lagradost.cloudstream3.Episode
 import com.lagradost.cloudstream3.HomePageList
 import com.lagradost.cloudstream3.HomePageResponse
+import com.lagradost.cloudstream3.LoadResponse
+import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
+import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
 import com.lagradost.cloudstream3.TvType
 import com.lagradost.cloudstream3.MainAPI
 import com.lagradost.cloudstream3.MainPageRequest
 import com.lagradost.cloudstream3.MovieSearchResponse
 import com.lagradost.cloudstream3.SearchResponse
+import com.lagradost.cloudstream3.SubtitleFile
 import com.lagradost.cloudstream3.app
 import com.lagradost.cloudstream3.mainPageOf
 import com.lagradost.cloudstream3.newHomePageResponse
+import com.lagradost.cloudstream3.newMovieLoadResponse
+import com.lagradost.cloudstream3.newTvSeriesLoadResponse
+import com.lagradost.cloudstream3.toRatingInt
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.INFER_TYPE
+import com.lagradost.cloudstream3.utils.Qualities
 import org.jsoup.nodes.Element
+import java.net.URI
+import java.net.URLDecoder
 
 class ExampleProvider(val plugin: TestPlugin) : MainAPI() {
     override var mainUrl = "https://vietsub.org"
+    private var directUrl = mainUrl
     override var name = "Vietsub"
     override val supportedTypes = setOf(TvType.Movie, TvType.Anime, TvType.TvSeries)
     override var lang = "vi"
@@ -52,7 +66,7 @@ class ExampleProvider(val plugin: TestPlugin) : MainAPI() {
     //    SEARCH PAGE
     override suspend fun search(query: String): List<SearchResponse> {
         return app.post(
-            "$mainUrl/search/$query/page/"
+            "$mainUrl/search/$query"
         ).document
             .select("article")
             .mapNotNull {
@@ -60,7 +74,7 @@ class ExampleProvider(val plugin: TestPlugin) : MainAPI() {
             }
     }
 
-    private fun Element.toSearchResponse(): MovieSearchResponse? {
+    private fun Element.toSearchResponse(): SearchResponse ? {
         val link = this.select("div.halim-item a").last() ?: return null
         val href = link.attr("href")
         val img = this.selectFirst("img")
@@ -71,11 +85,107 @@ class ExampleProvider(val plugin: TestPlugin) : MainAPI() {
             href,
             this@ExampleProvider.name,
             TvType.Movie,
-            fixUrl(img.attr("data-src")),
+            fixUrl(posterUrl),
         )
     }
 
     //    SHOW PAGE
+    override suspend fun load(url: String): LoadResponse {
+        val request = app.get(url)
+        directUrl = getBaseUrl(request.url)
+        val document = request.document
+
+        val title = document.selectFirst("strong")?.text()?.trim().toString()
+        val link = document.select("div.title-wrapper a").attr("href")
+        val poster = document.selectFirst("div.jw-preview.jw-reset")?.attr("style")?.let { Regex("""url\(["']?([^"']*)["']?\)""").find(it)?.groups?.get(1)?.value }
+        val tags = document.select("p.genres a").map { it.text() }
+        val year = document.select(".year").text().trim().toIntOrNull()
+        val tvType = if (document.select("#listsv-1 li").size > 1) TvType.TvSeries else TvType.Movie
+        val description = document.selectFirst(".item-content > p")?.text()?.trim().toString()
+        val rating =
+            document.select("span.score").text().toRatingInt()
+        val actors = document.select("p.actors a").map { it.text() }
+        val recommendations = document.select(".related-movies article").mapNotNull {
+            it.toSearchResponse()
+        }
+
+        return if (tvType == TvType.TvSeries) {
+            val episodes = document.select("#listsv-1 li").map {
+                val href = it.select("a").attr("href")
+                val episode =
+                    it.select("a > span").text().replace(Regex("[^0-9]"), "").trim().toIntOrNull()
+                val name = "Tập $episode"
+                Episode(
+                    data = href,
+                    name = name,
+                    episode = episode,
+                )
+            }
+
+            newTvSeriesLoadResponse(title, url, TvType.TvSeries, episodes) {
+                this.posterUrl = poster
+                this.year = year
+                this.plot = description
+                this.tags = tags
+                this.rating = rating
+                addActors(actors)
+                this.recommendations = recommendations
+            }
+        } else {
+            newMovieLoadResponse(title, url, TvType.Movie, link) {
+                this.posterUrl = poster
+                this.year = year
+                this.plot = description
+                this.tags = tags
+                this.rating = rating
+                addActors(actors)
+                this.recommendations = recommendations
+            }
+        }
+    }
+
+    override suspend fun loadLinks(
+        data: String,
+        isCasting: Boolean,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ): Boolean {
+        val document = app.get(data).document
+
+        val key = document.select("div#content script")
+            .find { it.data().contains("filmInfo.episodeID =") }?.data()?.let { script ->
+                val id = script.substringAfter("parseInt('").substringBefore("'")
+                app.post(
+                    url = "$directUrl/chillsplayer.php",
+                    data = mapOf("qcao" to id),
+                    referer = data,
+                    headers = mapOf(
+                        "X-Requested-With" to "XMLHttpRequest",
+                        "Content-Type" to "application/x-www-form-urlencoded; charset=UTF-8"
+                    )
+                ).text.substringAfterLast("iniPlayers(\"")
+                    .substringBefore("\"")
+            }
+
+        listOf(
+            Pair("https://sotrim.topphimmoi.org/raw/$key/index.m3u8", "PMFAST"),
+            Pair("https://dash.megacdn.xyz/raw/$key/index.m3u8", "PMHLS"),
+            Pair("https://so-trym.phimchill.net/dash/$key/index.m3u8", "PMPRO"),
+            Pair("https://dash.megacdn.xyz/dast/$key/index.m3u8", "PMBK")
+        ).map { (link, source) ->
+            callback.invoke(
+                ExtractorLink(
+                    source,
+                    source,
+                    link,
+                    referer = "$directUrl/",
+                    quality = Qualities.P1080.value,
+                    INFER_TYPE,
+                )
+            )
+        }
+        return true
+    }
 
     //    OTHER
     private fun fixUrl(url: String?): String {
@@ -89,4 +199,12 @@ class ExampleProvider(val plugin: TestPlugin) : MainAPI() {
 
         return mainUrl + url
     }
+
+    private fun getBaseUrl(url: String): String {
+        return URI(url).let {
+            "${it.scheme}://${it.host}"
+        }
+    }
+
+    private fun decode(input: String): String? = URLDecoder.decode(input, "utf-8")
 }
